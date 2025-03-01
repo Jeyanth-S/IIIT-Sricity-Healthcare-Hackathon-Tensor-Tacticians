d02054c9746c30e00351788c3e20ef244693d611
Jeyanth-S <jeya22018.ad@rmkec.ac.in>
2025-03-01 22:13:48 +0530

    Update API endpoint for text processing in app.py

--- models/app.py
+++ models/app.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://ehr-api-demo.loca.lt/process_text"
+url = "https://stupid-turkey-75.loca.lt/process_text"
 data = {"text": "Dr. Smith diagnosed John Doe, 40, with pneumonia and prescribed azithromycin."}
 
 response = requests.post(url, json=data)
